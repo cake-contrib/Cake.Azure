4bd8242d4aa90157e39f669aa8cb91b1d735ca9f
Dmytro Dziuma <dixond@acm.lviv.ua>
2017-04-09 15:16:36 +0300

    Added deployment alias

--- src/Cake.Azure/AzureResourceGroupAliases.cs
+++ src/Cake.Azure/AzureResourceGroupAliases.cs
@@ -4,6 +4,7 @@ using Cake.Core.Diagnostics;
 using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Rest;
+using Newtonsoft.Json.Linq;
 
 namespace Cake.Azure
 {
@@ -85,6 +86,44 @@ namespace Cake.Azure
             {
                 context.Log.Information($"Resource group '{resourceGroupName}' doesn't exist.");
             }
+        }
+
+        /// <summary>
+        /// Deploys resources to the resource group using the specified ARM template.
+        /// </summary>
+        /// <param name="context">The Cake context.</param>
+        /// <param name="credentials">The Azure credentials.</param>
+        /// <param name="subscriptionId">The subscription ID.</param>
+        /// <param name="resourceGroupName">The resource group name.</param>
+        /// <param name="deploymentName">The deployment name.</param>
+        /// <param name="template">The content of the ARM template file.</param>
+        /// <param name="parameters">The content of the ARM template parameters file.</param>
+        /// <returns>The outputs from the ARM template deployment.</returns>
+        [CakeAliasCategory("ResourceGroup")]
+        [CakeMethodAlias]
+        public static JObject DeployAzureResouceGroup(this ICakeContext context, ServiceClientCredentials credentials,
+            string subscriptionId, string resourceGroupName, string deploymentName, JObject template,
+            JObject parameters)
+        {
+            var client = GetClient(credentials, subscriptionId);
+
+            context.Log.Information($"Starting template deployment '{deploymentName}' in resource group '{resourceGroupName}'");
+            var deployment = new Deployment
+            {
+                Properties = new DeploymentProperties
+                {
+                    Mode = DeploymentMode.Incremental,
+                    Template = template,
+                    Parameters = parameters["parameters"].ToObject<JObject>()
+                }
+            };
+
+            var deploymentResult = client.Deployments.CreateOrUpdate(resourceGroupName,
+                deploymentName, deployment);
+
+            context.Log.Information($"Deployment status: {deploymentResult.Properties.ProvisioningState}");
+
+            return deploymentResult.Properties.Outputs as JObject;
         }
 
         private static ResourceManagementClient GetClient(ServiceClientCredentials credentials, string subscriptionId)
